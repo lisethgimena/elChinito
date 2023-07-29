0a8903fb92aff6e8991786684691d11499f7b531
lizethgimena <lisethadrianovalentin@gmail.com>
2023-07-29 18:53:24 -0500

    V3.04

---
+++ js/reserva.js
@@ -0,0 +1,20 @@
+/*
+let navbar = document.querySelector('.form');
+document.querySelector('#formSelectService1').onclick = () =>{
+    formTime.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+*/
+
+function selectService(){
+    if(form1.serviceOption[0].checked){
+        console.log(0);
+    }
+    if(form1.serviceOption[1].checked){
+        console.log(1);
+    }
+    if(form1.serviceOption[2].checked){
+        console.log(2);
+    }
+}

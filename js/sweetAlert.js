7182d52f719468b72350ed6907c3cb515e735274
Liseth Gimena <lisethadrianovalentin@gmail.com>
2023-08-03 17:03:11 -0500

    actualizada 3

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -23,7 +23,7 @@ document.querySelector('#boton1').addEventListener('click', function(){
   let horario = document.querySelector('#horario').value;
   let pago = document.querySelector('#pago').value;
 
-  let url = "https://wa.me/51912764864?text=*Buenas, quisiera hacer una reservación a:_* %0A *Nombre*:" +nombre + "%0A*Teléfono:* "
+  let url = "https://wa.me/51912764864?text=*_Buenas, quisiera hacer una reservación a:_* %0A*Nombre*:" +nombre + "%0A*Teléfono:* "
   +telefono +"%0A*DNI:* " +dni +"%0A*Servicio:* " +servicio +"%0A*Horario:* " 
   +horario +"%0A*Método de pago:*" +pago;
   window.open(url)

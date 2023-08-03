381a4fe33c7639f2b4bdcdeaac8168b826820a8c
Ferando <ferdapeva@gmail.com>
2023-08-04 00:20:20 +0200

    ajustado

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -23,15 +23,8 @@ let servicio = document.querySelector('#servicio').value;
 let horario = document.querySelector('#horario').value;
 let pago = document.querySelector('#pago').value;
 
-<<<<<<< HEAD
-let url = "https://wa.me/51912764864?text=Buenas, quisiera hacer una reservación a: Nombre: "
- +nombre +" Teléfono: "
-+telefono +" DNI: " +dni +" Servicio: " +servicio +" Horario: " 
-+horario +" Método de pago:" +pago;
-=======
 let url = "https://wa.me/51912764864?text=*_Buenas, quisiera hacer una reservación a:_* %0A*Nombre:* " +nombre +"%0A*Teléfono:* "
 +telefono +"%0A*DNI:* " +dni +"%0A*Servicio:* " +servicio +"%0A*Horario:* " 
 +horario +"%0A*Método de pago:*" +pago;
->>>>>>> b4a7d343617e6d863ab2eb7b6db4a61355c65511
 window.open(url)
 })

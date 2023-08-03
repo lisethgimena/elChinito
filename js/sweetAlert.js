e77b080d7a9500c73893e8772da1a7db5e1c2b7c
Liseth Gimena <lisethadrianovalentin@gmail.com>
2023-08-03 13:23:21 -0500

    Reservaciones

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -8,33 +8,23 @@ function enviar(){
       })
       
       swalWithBootstrapButtons.fire({
-        title: '¿Desea confirmar su reserva?',
-        text: "Verifique bien sus datos",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Sí, resérvalo!',
-        cancelButtonText: 'No, cancélalo!',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          swalWithBootstrapButtons.fire(
-            'Reservado',
-            'Sus datos fueron enviados con éxito. Gracias por su preferencia :)',
-            'success'
-          )
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          swalWithBootstrapButtons.fire(
-            'Cancelado',
-            'El envío fue cancelado con éxito',
-            'error'
-          )
-        }
-      })
+        title: 'Reservado',
+        text: "Sus datos fueron enviados con éxito. Gracias por su preferencia :)",
+        icon: 'success',
+        
+      })      
 }
 
-document.querySelector('result.isConfirmed').addEventListener('click', function(){
+document.querySelector('#boton1').addEventListener('click', function(){
   let nombre = document.querySelector('#nombre').value;
+  let telefono = document.querySelector('#telefono').value;
+  let dni = document.querySelector('#dni').value;
+  let servicio = document.querySelector('#servicio').value;
+  let horario = document.querySelector('#horario').value;
+  let pago = document.querySelector('#pago').value;
+
+  let url = "https://wa.me/51912764864?text=Buenas, quisiera hacer una reservación a: *_Nombre_*: " +nombre +" Teléfono: "
+  +telefono +" DNI: " +dni +" Servicio: " +servicio +" Horario: " 
+  +horario +" Método de pago:" +pago;
+  window.open(url)
 })

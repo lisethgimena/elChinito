33936fefd7b506c86f7cfc8cf55b788dce0f3bba
Liseth Gimena <lisethadrianovalentin@gmail.com>
2023-08-03 16:40:10 -0500

    modificada salvada xd

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,43 +1,9 @@
-function enviar() {
+function enviar(){
     const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
-            confirmButton: "btn btn-success",
-            cancelButton: "btn btn-danger",
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
         },
-<<<<<<< HEAD
-        buttonsStyling: false,
-    });
-
-    swalWithBootstrapButtons
-        .fire({
-            title: "¿Desea confirmar su reserva?",
-            text: "Verifique bien sus datos",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Sí, resérvalo!",
-            cancelButtonText: "No, cancélalo!",
-            reverseButtons: true,
-        })
-        .then((result) => {
-            if (result.isConfirmed) {
-                swalWithBootstrapButtons.fire(
-                    "Reservado",
-                    "Sus datos fueron enviados con éxito. Gracias por su preferencia :)",
-                    "success"
-                );
-            } else if (
-                /* Read more about handling dismissals below */
-                result.dismiss === Swal.DismissReason.cancel
-            ) {
-                swalWithBootstrapButtons.fire(
-                    "Cancelado",
-                    "El envío fue cancelado con éxito",
-                    "error"
-                );
-            }
-        });
-}
-=======
         buttonsStyling: false
       })
       
@@ -57,13 +23,8 @@ document.querySelector('#boton1').addEventListener('click', function(){
   let horario = document.querySelector('#horario').value;
   let pago = document.querySelector('#pago').value;
 
-<<<<<<< HEAD
-  let url = "https://wa.me/51912764864?text=*_Buenas, quisiera hacer una reservación a:_* %0A *Nombre*%0A: " +nombre +" Teléfono: "
-=======
-  let url = "https://wa.me/51912764864?text=*_Buenas, quisiera hacer una reservación a:_* 0A*Nombre*:0A " +nombre +" Teléfono: "
->>>>>>> e7f6c08c753ac17b307d3b6dfad7a5a1e874c127
+  let url = "https://wa.me/51912764864?text=*_Buenas, quisiera hacer una reservación a:_* %0A *Nombre*: %0A" +nombre +" Teléfono: "
   +telefono +" DNI: " +dni +" Servicio: " +servicio +" Horario: " 
   +horario +" Método de pago:" +pago;
   window.open(url)
 })
->>>>>>> 0962a445e628945ab0c7ae28efd9fc25b65f688a
